357b1ebce34f32f2af159a9bd48127bd02efba5c
hr7439 <104063334+hr7439@users.noreply.github.com>
2022-04-20 18:22:56 +0530

    Add files via upload

---
+++ github.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int amount,bill,quotient,reminder;
+    scanf("%d %d",&amount,&bill);
+    if (5<=amount<=2500 && 5<=bill<=2500)
+    {quotient=amount/bill;reminder=amount%bill; 
+    printf("%d\n",quotient);
+    printf("%d",reminder);}
+    else
+    {return 0;}   
+    return 0;
+}
